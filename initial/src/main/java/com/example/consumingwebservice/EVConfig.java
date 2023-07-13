6df134f89af30eee26f5e6c7d73f7698fe3c439a
DINMA <dinma@bcldb.com>
2023-07-13 11:51:23 -0700

    FC

---
+++ initial/src/main/java/com/example/consumingwebservice/EVConfig.java
@@ -0,0 +1,28 @@
+package com.example.consumingwebservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.consumingwebservice")
+public class EVConfig {
+
+  @Bean
+  public Jaxb2Marshaller marshaller() {
+    Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+    marshaller.setContextPath("com.example.consumingwebservice.wsdl");
+    return marshaller;
+  }
+
+  @Bean
+  public evClient evClient(Jaxb2Marshaller marshaller) {
+    evClient client = new evClient();
+    client.setDefaultUri(null);
+    client.setMarshaller(marshaller);
+    client.setUnmarshaller(marshaller);
+    return client;
+  }
+
+}
